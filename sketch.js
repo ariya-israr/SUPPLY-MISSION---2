c9c7bf665ba954048debe34fbb0baa968c132bac
ariya-israr <74014471+ariya-israr@users.noreply.github.com>
2021-01-29 18:30:38 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 
+	 redBox1 = new Box (403,648,200,20);
+	 redBox2 = new Box (503,610,20,100);
+	 redBox3 = new Box (296,610,20,100);
+
+	 packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.02, isStatic:true});
+	 World.add(world, packageBody);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  redBox1.display();
+  redBox2.display();
+  redBox3.display();
+  keyPressed();
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	Matter.Body.setStatic(packageBody,false);
+
+    
+  }
+}
+
+
+
